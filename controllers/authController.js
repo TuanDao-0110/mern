5da15b469555534976adb497059b0b0e82ad066d
tuan <rickytuan1991@gmail.com>
2022-11-13 16:43:23 +0200

    add verify

--- controllers/authController.js
+++ controllers/authController.js
@@ -47,14 +47,14 @@ const login = asyncHandler(async (req, res, next) => {
         process.env.REFRESH_TOKEN_SECRET,
         { expiresIn: '1d' }
     )
-    // create secure cookie with refesh token 
+    // create secure cookie with refesh token ==> this cookie will go along with response when send back to user. 
     res.cookie('jwt', refeshToken, {
         httpOnly: true,
         secure: true,// accessible only by web server
         sameSite: 'none', //cross-site cookie
         maxAge: 7 * 24 * 60 * 60 * 1000 // cookie expiry : Convenient option for setting the expiry time relative to the current time in milliseconds.
     })
-    // send accessToken containing username and roles
+    // send accessToken containing username and roles ==> 
     res.json({ accessToken })
 })
 
@@ -63,7 +63,41 @@ const login = asyncHandler(async (req, res, next) => {
 // @access Pulic -- because access token has expired
 
 const refresh = asyncHandler(async (req, res, next) => {
-
+    // if login success ==> they will have cookie with refresh token
+    const cookie = req.cookies
+    // in case refresh token ==> if cookie dont have cookie name jwt 
+    if (!cookie.jwt) {
+        return res.status(401).json({ msg: 'unathorized' })
+    }
+    // in case we request have cookie name which name jwt 
+    // we take that token from cookie
+    const refreshToken = cookie.jwt
+    jwt.verify(
+        // 1. factor is refreshToken from cookie
+        refreshToken,
+        // 2nd factor is secret key
+        process.env.REFRESH_TOKEN_SECRET,
+        // 3rd call back function
+        asyncHandler(async (err, decoded) => {
+            if (err) return res.status(403).json({ msg: 'Forbidden' })
+            const foundUser = await User.findOne({ username: decoded.username }).exec()
+            if (!foundUser) {
+                return res.status(401).json({ msg: 'unauthorized' })
+            }
+            // create new token
+            const accessToken = jwt.sign(
+                {
+                    "userInfor": {
+                        "username": foundUser.username,
+                        "roles": foundUser.roles
+                    }
+                },
+                process.env.ACCESS_TOKEN_SECRET,
+                { expiresIn: '10s' }
+            )
+            res.json({ accessToken })
+        })
+    )
 })
 
 // @desc logout
@@ -71,7 +105,14 @@ const refresh = asyncHandler(async (req, res, next) => {
 // @access Public just to clear cookie if exists
 
 const logout = asyncHandler(async (req, res, next) => {
-
+    const cookies = req.cookies
+    if (!cookies?.jwt) return res.sendStatus(204) // no Content
+    res.clearCookie('jwt', {
+        httpOnly: true,
+        sameSite: 'none',
+        secure: true,
+    })
+    res.json({ msg: 'Cookie cleared' })
 })
 
 
